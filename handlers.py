eef5fb97292cf7ef6e4a451e8d6f7f075224e049
Maksim-Borodin-Andreevich <maksimborodinandreev@gmail.com>
2024-07-05 15:45:39 +0300

    Probably last commit

--- handlers.py
+++ handlers.py
@@ -14,54 +14,13 @@ async def hello(message: Message):
 async def get_mood(message: Message):
 
     date_now = datetime.now().weekday()
-    if date_now == '4':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '5':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '6':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '0':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '1':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '2':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
-    if date_now == '3':
-        gif_from_pc = FSInputFile('meme-polish-cow.gif')
-        await message.answer_animation(
-            gif_from_pc,
-            caption=f'мой муд сегодня:',
-            show_caption_above_media=True,
-        )
+    path = './gifs/' + str(date_now) + '.gif'
+    gif_from_pc = FSInputFile(path=path)
+   
+    await message.answer_animation(
+        animation=gif_from_pc,
+        caption=f'мой муд сегодня:',
+        show_caption_above_media=True,
+    )
 
 
